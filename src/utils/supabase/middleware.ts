d23b0267b154766de77b0fbe024ebee9a1139e2e
Ariel <asolmon88@gmail.com>
2025-12-12 16:15:48 -0600

    potentially fixes redirect

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -39,7 +39,9 @@ export async function updateSession(request: NextRequest) {
 
   if (
     !user &&
-    !request.nextUrl.pathname.startsWith('/auth')
+    !request.nextUrl.pathname.startsWith('/auth') &&
+    !request.nextUrl.pathname.startsWith('/') &&
+    !request.nextUrl.pathname.startsWith('/seminario')
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
